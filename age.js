a8c9633c22280fc68f1e9157bf519012ad185147
Adhithiya21 <adhithiyaa21@gmail.com>
2024-11-04 10:04:55 +0530

    javascript

---
+++ age.js
@@ -0,0 +1,27 @@
+let age =54;
+if(age >=0 && age <12)
+{
+    console.log("child");
+    
+}
+else if(age >=13 && age <19)
+    {
+    console.log("Teenager");
+    
+}
+else if(age >=20 && age <35)
+    {
+    console.log("young Adult");
+    
+}
+else if(age >=36 && age <55)
+    {
+    console.log("Adult");
+    
+}
+
+else if(age >=56)
+    {
+    console.log("senior");
+    
+    }
